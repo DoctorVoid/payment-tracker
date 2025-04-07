194fab4c0a8221bc5ed693fb3e42cdf5923487cf
DoctorVoid <furmanmail@gmail.com>
2025-04-07 22:34:18 +0300

    feat: update package.json and vite.config.js for deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/payment-tracker/', // The repository name matches 'payment-tracker'
   plugins: [
     vue(),
     vueDevTools(),
